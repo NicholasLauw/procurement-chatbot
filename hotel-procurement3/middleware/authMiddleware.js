3a7ce2f5a268e2c7c2a3cb388408584e5baf53a4
NicholasLauw <156472890+NicholasLauw@users.noreply.github.com>
2026-06-30 13:10:42 +0700

    Delete hotel-procurement3 directory

--- hotel-procurement3/middleware/authMiddleware.js
+++
@@ -1,5 +0,0 @@
-function requireAuth(req, res, next) {
-  if (req.session?.user) return next();
-  return res.status(401).json({ error: 'Unauthorized. Please log in.' });
-}
-module.exports = { requireAuth };
